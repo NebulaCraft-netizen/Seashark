99cf32d6f9309a2b24642eea6d1db417d0c00b3a
NebulaCraft-netizen <nebulacraft101@gmail.com>
2025-09-30 14:32:06 +0500

    Update lexer.h

--- Seashark/include/lexer.h
+++ Seashark/include/lexer.h
@@ -1,1 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    PRINT, IDENT, NUMBER, STRING,
+    LPAREN, RPAREN, EQUAL, PLUS, MINUS, STAR, SLASH,
+    END, UNKNOWN
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+};
+
+class Lexer {
+public:
+    Lexer(const std::string &src);
+    std::vector<Token> tokenize();
+private:
+    std::string src;
+    size_t pos;
+    char peek();
+    char advance();
+    void skipWhitespace();
+};
 
